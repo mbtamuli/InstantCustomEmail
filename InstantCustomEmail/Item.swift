c33092a57c3dfab2c8db92b1e22ae5606fadbd35
Mriyam Tamuli <mbtamuli@gmail.com>
2024-11-14 19:16:14 +0530

    Setup project
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

---
+++ InstantCustomEmail/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  InstantCustomEmail
+//
+//  Created by Mriyam Tamuli on 14/11/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ InstantCustomEmailTests/InstantCustomEmailTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstantCustomEmailTests.swift
+//  InstantCustomEmailTests
+//
+//  Created by Mriyam Tamuli on 14/11/24.
+//
+
+import Testing
+@testable import InstantCustomEmail
+
+struct InstantCustomEmailTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
